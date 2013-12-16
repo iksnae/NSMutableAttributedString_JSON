30b0b7313cd3c301b44a18313f67ad7949666961
K <mustkre8@gmail.com>
2013-12-15 23:12:33 -0500

    added demo code to view controller
    Signed-off-by: K <mustkre8@gmail.com>

--- NSMutableAttributedString_JSON/ViewController.m
+++ NSMutableAttributedString_JSON/ViewController.m
@@ -1,15 +1,16 @@
 //
 //  ViewController.m
-//  NSMutableAttributedString_JSON
+//  JSONAttributedStringTest
 //
-//  Created by Khalid on 12/15/13.
-//  Copyright (c) 2013 Khalid. All rights reserved.
+//  Created by Khalid on 12/14/13.
+//
 //
 
 #import "ViewController.h"
+#import "NSMutableAttributedString+JSON.h"
 
 @interface ViewController ()
-
+@property (nonatomic,strong) UITextView * textView;
 @end
 
 @implementation ViewController
@@ -18,6 +19,14 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.textView = [[UITextView alloc]initWithFrame:CGRectMake(20, 40, 280, 400 )];
+    [self.view addSubview:self.textView];
+    NSData * jsonData = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"example" ofType:@"json"]];
+    NSDictionary * jsonDict = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:nil];
+    NSLog(@"jsonDict: %@",jsonDict);
+    NSMutableAttributedString * attString = [[NSMutableAttributedString alloc]initWithJSONDictionary:jsonDict];
+    [self.textView setFont:[UIFont italicSystemFontOfSize:36]];
+    [self.textView setAttributedText:[attString mutableCopy]];
 }
 
 - (void)didReceiveMemoryWarning
